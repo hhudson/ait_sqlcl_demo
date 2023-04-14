757cb0322aa50945f9d28eee2f20ab13c27fa431
Hayden Hudson <hayden.h.hudson@oracle.com>
2023-04-14 09:54:39 -0400

    init

---
+++ formatting/formatting_script.sql
@@ -0,0 +1,13 @@
+SQL> help set sqlformat
+
+SQL> set sqlformat insert
+
+SQL> select * from departments
+
+SQL> set sqlformat delimited
+
+SQL> set sqlformat json-formatted
+
+SQL> set sqlformat ansiconsole -config=highlight.json
+
+https://github.com/fusesource/jansi/blob/master/src/main/java/org/fusesource/jansi/Ansi.java
---
+++ sqlcl_script.sql
@@ -0,0 +1,16 @@
+SQL> help
+For help on a topic type help <topic>
+List of Help topics available:
+/                @                @@               ACCEPT           ALIAS            APEX             APPEND
+AQ               ARBORI           ARCHIVE_LOG      ARGUMENT         BREAK            BRIDGE           BTITLE
+CD               CHANGE           CLEAR            CLOUDSTORAGE     CODESCAN         COLUMN           COMPUTE
+CONNECT          CONNECTIONS      COPY             CS               CTAS             DATAPUMP         DBCCRED
+DDL              DEFINE           DEL              DESCRIBE         DG               DISCONNECT       EDIT
+EXECUTE          EXIT             FIND             FORMAT           GET              HISTORY          HOST
+INFORMATION      INPUT            LIQUIBASE        LIST             LOAD             MIGRATEADVISOR   MKSTORE
+MODELER          NET              OCI              OERR             ORAPKI           PASSWORD         PAUSE
+PRINT            PROMPT           QUIT             REMARK           REPEAT           RESERVED_WORDS   REST
+RUN              SAVE             SCRIPT           SET              SHOW             SHUTDOWN         SODA
+SPOOL            SSHTUNNEL        START            STARTUP          STORE            TIMING           TNSPING
+TOSUB            TTITLE           UNDEFINE         UNLOAD           VARIABLE         VAULT            WHENEVER
+WHICH            XQUERY
